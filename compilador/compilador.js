f001cdecbd34bb32ad3812e92c3b14ca4d0e4ca8
adsgabriel <63120824+adsgabriel@users.noreply.github.com>
2022-02-21 22:53:06 -0300

    add compilador

---
+++ compilador/compilador.js
@@ -0,0 +1,6 @@
+"use strict";
+let canal = 'xerife';
+let inscritos = 2245454;
+//canal = inscritos
+console.log(`Canal = ${canal}`);
+//let nome = 'Pedro'
---
+++ compilador/compilador.ts
@@ -0,0 +1,7 @@
+let canal: string = 'xerife'
+let inscritos: number = 2245454
+
+//canal = inscritos
+console.log(`Canal = ${canal}`)
+
+//let nome = 'Pedro'
