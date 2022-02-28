0c6952539527468bb5819e5f8ebaae8d5b6159e5
adsgabriel <63120824+adsgabriel@users.noreply.github.com>
2022-02-28 19:25:27 -0300

    fim Arrow Funcions

--- ecmascript/ecmascrip.js
+++ ecmascript/ecmascrip.js
@@ -19,3 +19,38 @@ console.log(sergredo);
 for (let i = 0; i < 10; i++) {
     console.log(i);
 }
+// Arrow functions
+function somar(n1, n2) {
+    return n1 + n2;
+}
+console.log(somar(2, 2));
+const subtrair = (n1, n2) => n1 - n2;
+console.log(subtrair(2, 3));
+const saudacao = () => console.log('ola');
+saudacao();
+const falarCom = (pessoa) => console.log('Ola ' + pessoa);
+falarCom('Silvio');
+//this
+/*function normalComThis() {
+    console.log(this)
+}
+normalComThis()
+
+const normalComThisEspecial = normalComThis.bind('this especial')
+normalComThisEspecial()
+
+//this?
+console.log(this)
+const arrowComThis = () => console.log(this)
+arrowComThis()*/
+//Parâmetros padrao
+function contagemRegressiva(inicio = 5, fim = inicio - 5) {
+    console.log(inicio);
+    while (inicio > fim) {
+        inicio--;
+        console.log(inicio);
+    }
+    console.log("Fim!");
+}
+contagemRegressiva();
+contagemRegressiva(3);
--- ecmascript/ecmascrip.ts
+++ ecmascript/ecmascrip.ts
@@ -25,3 +25,49 @@ console.log(sergredo);
 for(let i = 0; i < 10; i++){
     console.log(i)
 }
+
+
+// Arrow functions
+
+function somar(n1: number, n2: number): number {
+    return n1 + n2
+}
+console.log(somar(2,2));
+
+const subtrair = (n1: number, n2: number) => n1 - n2
+console.log(subtrair(2,3))
+
+const saudacao = () => console.log('ola')
+saudacao()
+
+const falarCom = (pessoa: string) => console.log('Ola ' + pessoa)
+falarCom('Silvio')
+
+//this
+
+/*function normalComThis() {
+    console.log(this)
+}
+normalComThis()
+
+const normalComThisEspecial = normalComThis.bind('this especial')
+normalComThisEspecial()
+
+//this?
+console.log(this)
+const arrowComThis = () => console.log(this)
+arrowComThis()*/
+
+//Parâmetros padrao
+
+function contagemRegressiva(inicio: number = 5, fim: number = inicio - 5): void {
+    console.log(inicio)
+    while(inicio > fim) {
+        inicio--
+        console.log(inicio)
+    }
+    console.log("Fim!")
+}
+
+contagemRegressiva()
+contagemRegressiva(3)
