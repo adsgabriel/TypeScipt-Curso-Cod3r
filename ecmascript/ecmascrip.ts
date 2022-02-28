e1e6101da08cf41a69cb3e0b0b40b399edcc854c
adsgabriel <63120824+adsgabriel@users.noreply.github.com>
2022-02-28 18:46:53 -0300

    Const e Let

---
+++ ecmascript/ecmascrip.ts
@@ -0,0 +1,27 @@
+// let & const
+let seraQuepode = 'P'
+console.log(seraQuepode)
+
+let estaFrio = true
+if (estaFrio) {
+    let acao = 'Colocar o casaco';
+    console.log(acao);
+}
+
+const cpf: string = '123.123.000-99'
+console.log(cpf)
+
+
+
+var sergredo = 'externo'
+function revelar() {
+    let sergredo = 'interno'
+    console.log(sergredo)
+}
+revelar();
+console.log(sergredo);
+
+
+for(let i = 0; i < 10; i++){
+    console.log(i)
+}
