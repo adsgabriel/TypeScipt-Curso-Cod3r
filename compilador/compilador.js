386de16fcff9073ad2f85d6d6ad91d92dcb878ff
adsgabriel <63120824+adsgabriel@users.noreply.github.com>
2022-02-28 10:53:11 -0300

    fim modulo compilaodr

--- compilador/compilador.js
+++ compilador/compilador.js
@@ -4,3 +4,19 @@ let inscritos = 2245454;
 //canal = inscritos
 console.log(`Canal = ${canal}`);
 //let nome = 'Pedro'
+function soma(a, b) {
+    return a + b;
+}
+let qualquerCoisa;
+qualquerCoisa = 12;
+qualquerCoisa = 'abc';
+function saudar(isManha) {
+    let saudacao;
+    if (isManha) {
+        saudacao = 'Bom dia';
+    }
+    else {
+        saudacao = 'Bom';
+    }
+    return saudacao;
+}
--- compilador/compilador.ts
+++ compilador/compilador.ts
@@ -5,3 +5,22 @@ let inscritos: number = 2245454
 console.log(`Canal = ${canal}`)
 
 //let nome = 'Pedro'
+
+function soma(a: any, b: any) {
+    return a + b
+}
+
+let qualquerCoisa
+qualquerCoisa = 12;
+qualquerCoisa = 'abc';
+
+
+function saudar(isManha: boolean) {
+    let saudacao: string
+    if (isManha) {
+        saudacao = 'Bom dia'
+    }else{
+        saudacao = 'Bom'
+    }
+    return saudacao
+}
