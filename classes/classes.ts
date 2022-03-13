f5383e1a1dc8da7b1235717601d479761d5211f2
adsgabriel <63120824+adsgabriel@users.noreply.github.com>
2022-03-13 13:00:10 -0300

    add modificadores de acesso

--- classes/classes.ts
+++ classes/classes.ts
@@ -44,13 +44,55 @@ class Produto{
     constructor(public nome: string, public preco: number, public desconto: number = 0){
     }
 
+    precoComDesconto(): number{
+        return this.preco * (1 - this.desconto)
+    }
+
+    public resumo(): string{
+        return `${this.nome} custa R$${this.precoComDesconto} (${this.desconto * 100}%off)`
+    }
 }
 
 const produto1 = new Produto('Iphone 13',  6000);
 produto1.desconto = 0.06
-console.log(produto1);
+console.log(produto1.resumo());
 
 const produto2 = new Produto('Macbook Pro', 15000, 0.2)
 console.log(produto2);
 
+class Carro{
+    private velocidadeAtual: number = 0
+
+    constructor(public marca: string, public modelo: string,
+        private velocidadeMaxima: number = 200) {
+
+        }
+
+        private alterarVelocidade(delta: number): number {
+            const novaVelocidade = this.velocidadeAtual + delta
+            const velocidadeValida = novaVelocidade >= 0
+            && novaVelocidade <= this.velocidadeMaxima
+
+            if(velocidadeValida){
+                this.velocidadeAtual = novaVelocidade
+            }else{
+                this.velocidadeAtual = delta > 0 ? this.velocidadeMaxima : 0
+            }
+
+            return this.velocidadeAtual;
+        }
+
+        public acelerar(): number{
+            return this.alterarVelocidade(5);
+        }
+
+        public frear(): number {
+            return this.alterarVelocidade(-5);
+        }
+}
+
+const carro1 = new Carro('Ford', 'Mustang', 250);
+
+Array(50).fill(0).forEach(() => carro1.acelerar())
+console.log(carro1.acelerar());
 
